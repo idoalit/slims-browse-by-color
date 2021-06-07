293868e61d96a9814ef0f32e771f9de975dc93b1
idoalit <ido.alit@gmail.com>
2021-06-07 11:57:42 +0700

    Change color group algoritm

--- lib/BiblioColour.php
+++ lib/BiblioColour.php
@@ -55,7 +55,8 @@ class BiblioColour
         return null;
     }
 
-    static function saveColor($color, $biblio_id) {
+    static function saveColor($color, $biblio_id)
+    {
         // save to database
         $dominants = [];
         foreach ($color['palette'] as $item) {
@@ -68,15 +69,16 @@ class BiblioColour
         // check if data exist
         $_query = DB::getInstance('mysqli')->query('SELECT color_palette, color_dominant FROM biblio_custom WHERE biblio_id = ' . $biblio_id);
         if ($_query->num_rows > 0) {
-            $save = DB::getInstance('mysqli')->query("UPDATE `biblio_custom` SET `color_palette` = '".$palette."', `color_dominant` = '".$dominant."' WHERE `biblio_id` = '".$biblio_id."';");
+            $save = DB::getInstance('mysqli')->query("UPDATE `biblio_custom` SET `color_palette` = '" . $palette . "', `color_dominant` = '" . $dominant . "' WHERE `biblio_id` = '" . $biblio_id . "';");
         } else {
-            $save = DB::getInstance('mysqli')->query("INSERT INTO `biblio_custom` (`biblio_id`, `color_palette`, `color_dominant`) VALUES ('".$biblio_id."', '".$palette."', '".$dominant."');");
+            $save = DB::getInstance('mysqli')->query("INSERT INTO `biblio_custom` (`biblio_id`, `color_palette`, `color_dominant`) VALUES ('" . $biblio_id . "', '" . $palette . "', '" . $dominant . "');");
         }
 
         return $save;
     }
 
-    static function extractSaveById($biblio_id) {
+    static function extractSaveById($biblio_id)
+    {
         $query = DB::getInstance('mysqli')->query('SELECT image FROM biblio WHERE biblio_id = ' . $biblio_id);
         $data = $query->fetch_assoc();
         // get colors
@@ -89,30 +91,85 @@ class BiblioColour
 
     static function groupOf($rgb): ?string
     {
-        list($red, $green, $blue) = $rgb;
+        $hsv = self::rgb2hsv($rgb);
+        $hue = $hsv[0];
 
         #white
-        if ($red > 240 && $green > 240 && $blue > 240) return 'white';
+        if ($hsv[1] <= 5 && $hsv[2] >= 95) return 'white';
         #black
-        if (($red < 82 && $green < 82 && $blue < 82) || (abs($red - $green) < 10 && abs($green - $blue) < 10)) return 'black';
-        #yellow
-        if ($blue < $red && $blue < $green && abs($red - $green) < 10 && $red >= 160 && $green >= 160 && $blue < 100) return 'yellow';
-        #orange
-        if ($red > $green && $green > $blue && $red > 200 && $green > 126 && $blue < 20) return 'orange';
+        if ($hsv[2] <= 10) return 'black';
         #red
-        if ($red > $green && $red > $blue && $red > 150 && $green < 70 && $blue < 70) return 'red';
-        #purple
-        if ($blue > $red && $red > $green) return 'purple';
-        #magenta
-        if (abs($blue - $red) < 10 && $green < $red) return 'magenta';
+        if ($hue < 10 || $hue > 345) return 'red';
+        #orange
+        if ($hue < 40 && $hue > 10) return 'orange';
+        #yellow
+        if ($hue < 60 && $hue > 40) return 'yellow';
         #green
-        if ($green > $red && $green > $blue) return 'green';
+        if ($hue < 160 && $hue > 60 && $hsv[1] >= 30) return 'green';
         #teal
-        if (abs($blue - $green) < 10 && $red < $blue) return 'teal';
+        if ($hue < 180 && $hue > 160) return 'teal';
         #blue
-        if ($blue > $green && $blue > $red && $blue > 150) return 'blue';
+        if ($hue < 250 && $hue > 180) return 'blue';
+        #purple
+        if ($hue < 310 && $hue > 250) return 'yellow';
+        #magenta
+        if ($hue < 345 && $hue > 310) return 'yellow';
 
         # uncategorized
         return null;
+    }
+
+    /**
+     * @source: https://stackoverflow.com/a/13887939
+     */
+    static function rgb2hsv($rgb)    // RGB values:    0-255, 0-255, 0-255
+    {                                // HSV values:    0-360, 0-100, 0-100
+        list($R, $G, $B) = $rgb;
+        // Convert the RGB byte-values to percentages
+        $R = ($R / 255);
+        $G = ($G / 255);
+        $B = ($B / 255);
+
+        // Calculate a few basic values, the maximum value of R,G,B, the
+        //   minimum value, and the difference of the two (chroma).
+        $maxRGB = max($R, $G, $B);
+        $minRGB = min($R, $G, $B);
+        $chroma = $maxRGB - $minRGB;
+
+        // Value (also called Brightness) is the easiest component to calculate,
+        //   and is simply the highest value among the R,G,B components.
+        // We multiply by 100 to turn the decimal into a readable percent value.
+        $computedV = 100 * $maxRGB;
+
+        // Special case if hueless (equal parts RGB make black, white, or grays)
+        // Note that Hue is technically undefined when chroma is zero, as
+        //   attempting to calculate it would cause division by zero (see
+        //   below), so most applications simply substitute a Hue of zero.
+        // Saturation will always be zero in this case, see below for details.
+        if ($chroma == 0)
+            return array(0, 0, $computedV);
+
+        // Saturation is also simple to compute, and is simply the chroma
+        //   over the Value (or Brightness)
+        // Again, multiplied by 100 to get a percentage.
+        $computedS = 100 * ($chroma / $maxRGB);
+
+        // Calculate Hue component
+        // Hue is calculated on the "chromacity plane", which is represented
+        //   as a 2D hexagon, divided into six 60-degree sectors. We calculate
+        //   the bisecting angle as a value 0 <= x < 6, that represents which
+        //   portion of which sector the line falls on.
+        if ($R == $minRGB)
+            $h = 3 - (($G - $B) / $chroma);
+        elseif ($B == $minRGB)
+            $h = 1 - (($R - $G) / $chroma);
+        else // $G == $minRGB
+            $h = 5 - (($B - $R) / $chroma);
+
+        // After we have the sector position, we multiply it by the size of
+        //   each sector's arc (60 degrees) to obtain the angle in degrees.
+        $computedH = 60 * $h;
+
+        return array($computedH, $computedS, $computedV);
     }
 }
